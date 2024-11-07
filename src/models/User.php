ffbe90b1d34699f0e32d7565e9abe038819f3fe3
belezyak <LubovBelezyak@yandex.ru>
2024-11-08 00:31:42 +0500

    init

---
+++ src/models/User.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Src\Models;
+
+use Src\Services\Database;
+
+class User
+{
+    private $table = 'users';
+    private $id;
+    private $first_name;
+    private $last_name;
+    private $email;
+    private $password;
+    private $created_at;
+    private $updated_at;
+
+    private $connection;
+
+    public function __construct($db)
+    {
+        $this->connection = $db;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @param mixed $first_name
+     */
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param mixed $last_name
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    public function find($id) {
+        $query = "SELECT id, first_name, last_name, email FROM " . $this->table . " WHERE id=:id";
+        $connection = $this->connection->prepare($query);
+        $connection->bindParam(':id', $id);
+        $connection->execute();
+        return $connection;
+    }
+
+    public function create() {
+        $query = "INSERT INTO " . $this->table . " SET first_name=:first_name, last_name=:last_name, email=:email, password=:password";
+        $stmt = $this->connection->prepare($query);
+
+        $this->first_name = htmlspecialchars(strip_tags($this->first_name));
+        $this->last_name = htmlspecialchars(strip_tags($this->last_name));
+        $this->password = password_hash(htmlspecialchars(strip_tags($this->first_name)), PASSWORD_DEFAULT);
+        $this->email = htmlspecialchars(strip_tags($this->email));
+
+        $stmt->bindParam(":first_name", $this->first_name);
+        $stmt->bindParam(":last_name", $this->last_name);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":password", $this->password);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function update() {
+        $query = "UPDATE " . $this->table . " SET first_name=:first_name, last_name=:last_name, email=:email, password=:password WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+
+        $this->first_name = htmlspecialchars(strip_tags($this->first_name));
+        $this->last_name = htmlspecialchars(strip_tags($this->last_name));
+        $this->password = password_hash(htmlspecialchars(strip_tags($this->first_name)), PASSWORD_DEFAULT);
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(":first_name", $this->first_name);
+        $stmt->bindParam(":last_name", $this->last_name);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":password", $this->password);
+        $stmt->bindParam(':id', $this->id);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function delete() {
+        $query = "DELETE FROM " . $this->table . " WHERE id = :id";
+        $stmt = $this->connection->prepare($query);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(':id', $this->id);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+}
